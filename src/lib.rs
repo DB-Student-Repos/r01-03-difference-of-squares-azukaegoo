723aa54cb60ec9bbea8f8a332f0172bb8365c8f5
Gloria Azum <azumgloria@gmail.com>
2024-03-17 23:03:53 +0100

    updated to formula usage

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 pub fn square_of_sum(n: u32) -> u32 {
-
-    let sum: u32 = (1..=n).sum();
+    let sum = n * (n + 1) / 2;
     sum * sum
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
 
-    (1..=n).map(|x| x * x).sum()
+   let sum_sqr = n * (n + 1) * (2 * n + 1) / 6;
+    sum_sqr
 }
 
 pub fn difference(n: u32) -> u32 {
